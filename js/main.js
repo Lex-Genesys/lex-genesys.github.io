028e20f3b1e61b62ece558532ad57a3aa652abc4
Lex-Genesys <122012971+Lex-Genesys@users.noreply.github.com>
2023-07-20 14:51:26 +0200

    update

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@
         return false;
     });
     
-    
+
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
@@ -62,11 +62,8 @@
 
 var callbackButton = document.getElementById('buttonCallback');
 
-
-
-
-
 callbackButton.addEventListener('click', function(event) {
+    var xhr = new XMLHttpRequest(); // Declare and initialize the xhr variable here
 
     var name = document.getElementById('Name').value;
     var phone = document.getElementById('phone').value;
@@ -74,8 +71,6 @@ callbackButton.addEventListener('click', function(event) {
     console.log(date);
 
     alert('Le bouton de rappel a été cliqué !');
-
-    // Création d'une instance de XMLHttpRequest
 
     // URL de la requête
     var url = "https://api.mypurecloud.de/api/v2/conversations/callbacks";
@@ -86,69 +81,34 @@ callbackButton.addEventListener('click', function(event) {
         "callbackUserName": name,
         "callbackNumbers": [phone]
     };
-        
-      
     var requestBodyJson = JSON.stringify(requestBody);
 
     // Ouvrir la requête avec la méthode "POST" et l'URL
     xhr.open("POST", url);
 
     // Définir les en-têtes appropriées
-    var authToken = "i49RmwvN-RCwdINzn99BR_-ad5fOJFH3HrypXqAsNv8PU_zeGMd8I8_QN1yIcNuO1nj5fKCjMeLP1qy18DZ85g"; 
+    var authToken = "i49RmwvN-RCwdINzn99BR_-ad5fOJFH3HrypXqAsNv8PU_zeGMd8I8_QN1yIcNuO1nj5fKCjMeLP1qy18DZ85g";
     xhr.setRequestHeader("Authorization", "Bearer " + authToken);
-    xhr.setRequestHeader("Content-Type", "application/json"); 
+    xhr.setRequestHeader("Content-Type", "application/json");
     xhr.setRequestHeader("Accept", "application/json");
 
     // Gérer l'événement de réponse
     xhr.onload = function () {
         if (xhr.status >= 200 && xhr.status < 300) {
-        // La requête a réussi, gérer la réponse ici si nécessaire
-        var response = JSON.parse(xhr.responseText);
-        console.log(response);
-    } else {
-        
-        // La requête a échoué, gérer l'erreur ici si nécessaire
-         console.error("La requête a échoué avec le statut :", xhr.status);
-     }
+            // La requête a réussi, gérer la réponse ici si nécessaire
+            var response = JSON.parse(xhr.responseText);
+            console.log(response);
+        } else {
+            // La requête a échoué, gérer l'erreur ici si nécessaire
+            console.error("La requête a échoué avec le statut :", xhr.status);
+        }
     };
-
 
     // Gérer l'erreur de la requête
     xhr.onerror = function () {
-    console.error("Erreur de requête");
+        console.error("Erreur de requête");
     };
 
     // Envoyer la requête avec le corps JSON
     xhr.send(requestBodyJson);
-
-
-
-/*
-    var headers = {
-        "Authorization": "bearer r08njAcY7N_fiOxbcm2IJe4LDai3mGocbGJqb4b2yg6eJxjdX2ayeFIxVmZBQxatppe1EZuSeML6Q4KcOIf_tA",
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-
-    var jsonBody = JSON.stringify(
-        {
-            "queueId":"636f560a-bc92-45e4-a8c3-79b53dd7f817",
-            "callBackUserName":name,
-            "callbackNumbers": [phone]
-        }
-    )
-const request = new Request("https://api.mypurecloud.de/api/v2/conversations/callbacks", {
-        method: "POST",
-        body: {
-            "queueId": "636f560a-bc92-45e4-a8c3-79b53dd7f817",
-            "callbackUserName": "Amira",
-            "callbackNumbers": ["1234"],
-         },
-        headers:headers
-    })
-    fetch(request)
-.then((response) => {
-}) 
-// ,*/
-    //PureCloud.notificationChannel.addNotificationCallback('button.callback.clicked', handleCallback);
-  });
+});
