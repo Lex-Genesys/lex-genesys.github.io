1a4fb53aef57b2364a3b4fc505f553a58a9bfb6d
Lex-Genesys <122012971+Lex-Genesys@users.noreply.github.com>
2023-07-20 14:42:02 +0200

    update

--- js/main.js
+++ js/main.js
@@ -32,15 +32,7 @@
         return false;
     });
     
-
-    // Date and time picker
-    $('.date').datetimepicker({
-        format: 'L'
-    });
-    $('.time').datetimepicker({
-        format: 'LT'
-    });
-
+    
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
