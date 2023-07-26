05af23c4e8fc9dd35f533993afddf0e117a76256
Lex-Genesys <122012971+Lex-Genesys@users.noreply.github.com>
2023-07-26 14:18:35 +0200

    update

--- js/main.js
+++ js/main.js
@@ -84,13 +84,6 @@ dateTimeInput.addEventListener('click', showCalendarOnClick);
 function handleInitialDateTimeField() {
     handleDateTimeField(); // Call the function to set the initial state
  }
-const dateTimeInput = document.getElementById('DateTime');
-dateTimeInput.addEventListener('click', showCalendarOnClick);
-
-// Function to handle the initial state of the date-time field on page load
-function handleInitialDateTimeField() {
-    handleDateTimeField(); // Call the function to set the initial state
-}
 
 // Attach event listener to the "DOMContentLoaded" event to handle initial state
 document.addEventListener('DOMContentLoaded', handleInitialDateTimeField);
